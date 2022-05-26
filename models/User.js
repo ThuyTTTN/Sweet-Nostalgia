3bf07e4138ecb885ffd15da095be8f6e78849397
Thuy Nguyen <ttruong06@gmail.com>
2022-05-26 15:14:40 -0400

    user-model

---
+++ models/User.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require("sequelize");
+const { model } = require("../config/connection");
+const sequelize = require("../config/connection");
+
+//create our user model
+class user extends Model {}
+
+//define table columns and configuration
+user.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.pasword = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
+
+    sequelize,
+    timestamp: false,
+    freezeTableName: true,
+    underscore: true,
+    modelName: "user",
+  }
+);
+
+model.exports = User;
