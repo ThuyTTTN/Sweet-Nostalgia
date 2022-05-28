7722bee268446180ee2caf2c4b89bffafc7acfd0
Brainybrian316 <brainybrian316@gmail.com>
2022-05-28 11:54:03 -0400

    added sess variable in index and got login page to display

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,29 +1,28 @@
 // modules required for routing
 const router = require('express').Router();
-// const { User } = require('../models');
-const sequelize = require('../config/connection');
+// const { User, Candy } = require('../models');
 
-// // GET signup page
-// router.get('/signup', (req, res) => { // we are checking if the user is logged in
-//     if (req.session.loggedIn) {
-//         // if they are logged in, redirect them to the home page
-//         res.redirect('/');
-//         return;
-//     }
-//     // render the signup.handlebars file if the user is not logged in
-//     res.render('signup');
-// });
+// GET signup page
+router.get('/signup', (req, res) => { // we are checking if the user is logged in
+    if (req.session.loggedIn) {
+        // if they are logged in, redirect them to the home page
+        res.redirect('/');
+        return;
+    }
+    // render the signup.handlebars file if the user is not logged in
+    res.render('signup');
+});
 
-// // GET login page
-// router.get('/login', (req, res) => { //we are checking if the user is logged in
-//     if (req.session.loggedIn) {
-//         // if they are logged in, redirect them to the home page
-//         res.redirect('/');
-//         return;
-//     }
-//     // render the login.handlebars file if the user is not logged in
-//     res.render('login');
-// });
+// GET login page
+router.get('/login', (req, res) => { //we are checking if the user is logged in
+    if (req.session.loggedIn) {
+        // if they are logged in, redirect them to the home page
+        res.redirect('/');
+        return;
+    }
+    // render the login.handlebars file if the user is not logged in
+    res.render('login');
+});
 
 //  GET home page
 router.get('/', (req, res) => { // anyone can access this page
