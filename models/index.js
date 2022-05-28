96c936c8cb4b11c5e68cb5148e370d6dadb3752e
Thuy Nguyen <ttruong06@gmail.com>
2022-05-28 11:04:12 -0400

    updated candy model and added user/candy associations in index.js model

--- models/index.js
+++ models/index.js
@@ -3,7 +3,14 @@ const Candy = require('./Candy');
 
 
 //create associations
+User.hasMany(Candy, {
+    foreignKey: 'user_id',
+});
+    
 
+Candy.hasMany(User, {
+    foreignKey: 'candy_id'
+});
 
 
 module.exports = { User, Candy };
