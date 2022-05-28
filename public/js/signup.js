fbf2ad22e4ec1cdc7e1a12d67237e439e62250b1
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-28 01:19:37 -0400

    completed signup.js and handlebars

---
+++ public/js/signup.js
@@ -0,0 +1,37 @@
+async function signupFormHandles (event){
+    event.preventDefault();
+
+    const first_name = document.querySelector('#first-input-signup');
+    const last_name = document.querySelector('#last-input-signup');
+    const email = document.querySelector('#email-input-signup');
+    const password = document.querySelector('#password-input-signup');
+    const address = document.querySelector('#address-input-signup');
+    const city = document.querySelector('#city-input-signup');
+    const state = document.querySelector('#state-input-signup');
+    const zipCode = document.querySelector('#zip-input-signup');
+
+
+    if(first_name && last_name && email && password && address && city && state && zipCode){
+        const signupApi = await fetch('/api/users',{
+            method:'POST',
+            body:JSON.stringify({
+                first_name,
+                last_name,
+                email,
+                password,
+                address,
+                city,
+                state,
+                zipCode
+            }),
+            headers:{'Content-Type':'application/json'},
+        });
+        if(signupApi.ok){
+            document.location.replace('/dashboard');
+        }else{
+            alert('Faliure to sign up :(');
+        }
+    };
+};
+
+document.querySelector('.signup-form').addEventListener('submit',signupFormHandles);
