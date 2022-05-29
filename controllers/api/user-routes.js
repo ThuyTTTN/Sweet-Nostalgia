4b0096f856345818d37713f6071c5997f3d93a94
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-05-29 17:33:31 -0400

    Updated develop

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,8 +45,14 @@ router.get('/:id', (req, res) => {
 // POST /api/users
 router.post('/', (req, res) => {
     User.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,        
         email: req.body.email,
-        password: req.body.password
+        password: req.body.password,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        zipCode: req.body.zipCode
     })
     .then(dbUserData => {
 
@@ -64,73 +70,18 @@ router.post('/', (req, res) => {
     });
 });
 
-// LOGIN USER /api/users/login
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: {
-            email: req.body.email 
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that email' });
-            return;
-        }
-        const validPassword = dbUserData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.user_id = dbUserData.id;
-            req.session.email = dbUserData.email;
-            req.session.loggedIn = true;
-      
-            res.json({ user: dbUserData, message: 'You are now logged in' });
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
 
 
-// LOGOUT USER /api/users/logout
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    }
-    else {
-        res.status(404).end();
-    }
-});
+// LOGIN USER /api/users/login HERE...
 
-// PUT /api/users/1
-router.put('/:id', (req, res) => {
-    User.update(req.body, {
-        individualHooks: true,
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData[0]) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
 
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+
+
+// LOGOUT USER /api/users/logout HERE...
+
+
+
+
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
