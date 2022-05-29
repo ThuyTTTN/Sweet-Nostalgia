154795d749cf1216e2625b67148782b3a97f930c
Brainybrian316 <brainybrian316@gmail.com>
2022-05-29 11:28:44 -0400

    routes work as expected

--- models/index.js
+++ models/index.js
@@ -6,12 +6,29 @@ const Candy = require('./Candy');
 //create associations
 /* --- to be used later possibly --- */
 User.hasOne(Candy);
+// *this works but is not ideal because it is not a one to many relationship maybe?
+// User.hasMany(Candy);
 
-User.belongsTo(Candy);
+// User.belongsTo(Candy);
 
 Candy.belongsTo(User);
 
-Candy.hasMany(User);
+// Candy.hasMany(User);
+
+// User.hasMany(Candy, {
+// });
+
+// ! same as above
+// User.belongsTo(Candy, {
+// });
+    
+
+// Candy.hasMany(User, {
+// });
+
+// Candy.belongsTo(User, {
+// });
+
 
 
 
