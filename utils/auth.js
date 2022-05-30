b44e22ef654f85cd00e8e5118ef1ccb08545d815
Brainybrian316 <brainybrian316@gmail.com>
2022-05-30 12:55:35 -0400

    undid testing changes

--- utils/auth.js
+++ utils/auth.js
@@ -1,28 +1,14 @@
-// //  this will check if the user is logged in before running the "next" arrow function of the route
-// const withAuth = (req, res, next) => {
-//       // if the user is logged in
-//       if (!req.session.id) {
-//            // if the user is not logged in we redirect to the login page
-//           res.redirct('/login');
-//           return;
-//       } else {
-//               // if the user is logged in we run the next arrow function for the route
-//         next();
-//       }
-// }
-
-// module.exports = withAuth;
-
 //  this will check if the user is logged in before running the "next" arrow function of the route
 const withAuth = (req, res, next) => {
       // if the user is logged in
-      if (!req.session.user_id) { // if the user is not logged in we redirect to the login page
-          res.redirect('/login');
+      if (!req.session.id) {
+           // if the user is not logged in we redirect to the login page
+          res.redirct('/login');
           return;
       } else {
-          // if the user is logged in we run the next arrow function for the route
-          next();
+              // if the user is logged in we run the next arrow function for the route
+        next();
       }
-  };
-  
-  module.exports = withAuth;
+}
+
+module.exports = withAuth;
