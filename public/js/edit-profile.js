12b91541109b0ba28d49f9d8f38dad639e74c563
Brainybrian316 <brainybrian316@gmail.com>
2022-06-03 19:52:14 -0400

    password functionality works and refactoring code

--- public/js/edit-profile.js
+++ public/js/edit-profile.js
@@ -38,32 +38,3 @@ async function editFormHandler(event) {
 }
 
 document.querySelector('.edit-profile-form').addEventListener('submit', editFormHandler);
-
-
-async function editPasswordHandler(event) {
-    event.preventDefault();
-
-    const password = document.querySelector('#edit-password').value;
-    const confirmPassword = document.querySelector('#edit-confirm-password').value;
-
-    if (password !== confirmPassword) {
-        alert('Passwords do not match');
-        return;
-    } 
-
-    const response = await fetch(`/api/users/`, {
-        method: 'put',
-        body: JSON.stringify({
-            password,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
-}
-
-document.querySelector('.edit-password-form').addEventListener('submit', editPasswordHandler);
