dcdf829463fe5668308f4edb853ceac2fe60e6a5
Brainybrian316 <brainybrian316@gmail.com>
2022-06-03 00:35:36 -0400

    working on edit route

--- public/js/edit-profile.js
+++ public/js/edit-profile.js
@@ -13,8 +13,7 @@ async function editFormHandler(event) {
 
 
     // variable for teh id of the user using the url
-    const id = window.location.toString().split('/')
-    [window.location.toString().split('/').length - 1];
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
     const response = await fetch(`/api/users/${id}`, {
         method: 'put',
