8a9ee8bd85a70bf2ccba4a5ecd4a1a608bac0726
Brainybrian316 <brainybrian316@gmail.com>
2022-06-02 22:47:19 -0400

    basic edit-profile.js handler

---
+++ public/js/edit-profile.js
@@ -0,0 +1,35 @@
+// a syncrhonous function to get the user's profile
+async function editFormHandler(event) {
+    //  preventDefault method prevents the form from submitting
+    event.preventDefualt();
+
+    const  first_name = document.querySelector('#firstName').value;
+    const  last_name = document.querySelector('#lastName').value;
+    const  email = document.querySelector('#email').value;
+    const address = document.querySelector('#address').value;
+    const city = document.querySelector('#city').value;
+    const state = document.querySelector('#state').value;
+    const zipCode = document.querySelector('#zipCode').value;
+
+    // variable for teh id of the user using the url
+    const id = window.location.toString().split('/')
+    [window.location.toString().split('/').length - 1];
+
+    const response = await fetch(`/api/users/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            first_name,
+            last_name,
+            email,
+            password,
+            address,
+            city,
+            state,
+            zipCode,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+   
+}
+
