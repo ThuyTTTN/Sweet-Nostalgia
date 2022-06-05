2e64b8ae1e1e229224e2f53e4bff9c9301e52ba2
Brainybrian316 <brainybrian316@gmail.com>
2022-06-05 14:49:03 -0400

    working site: :

--- controllers/api/users-routes.js
+++ controllers/api/users-routes.js
@@ -119,7 +119,7 @@ router.delete('/:id', (req, res) => {
   })
   .then(dbUserData => {
     if (!dbUserData) {
-      res.status(404).json({ message: 'No tag found with this id' });
+      res.status(404).json({ message: 'No user found with this id' });
       return;
     }
     res.json(dbUserData);
